6bbb71f296c878f6a0ec6c0032292abad1cd39d9
mohsennikfarjam <mohsennikfarjam@example.com>
2026-04-13 15:09:16 -0500

    Initialize Part 1 simple-interest.sh for mohsennikfarjam

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,9 +2,7 @@
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
-# Addtional Authors:
-# <your Github username>
+# Author: mohsennikfarjam
 
 # Input:
 # p, principal amount
